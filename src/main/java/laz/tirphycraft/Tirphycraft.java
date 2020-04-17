05d9ea75c594307b92e01b52e7d5624f06481027
Drizzs <drizzs@hotmail.com>
2020-04-17 15:29:35 -0400

    Start

---
+++ src/main/java/laz/tirphycraft/Tirphycraft.java
@@ -0,0 +1,31 @@
+package laz.tirphycraft;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+import static laz.tirphycraft.Tirphycraft.MOD_ID;
+
+@Mod(MOD_ID)
+public class Tirphycraft
+{
+    public static final String MOD_ID = "tirphycraft";
+
+    public Tirphycraft() {
+
+    }
+}
