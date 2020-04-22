d529dbbc706fdc4d34c3c1405c655eac14c0f6ce
Drizzs <drizzs@hotmail.com>
2020-04-22 14:26:04 -0400

    Event for laz

--- src/main/java/laz/tirphycraft/Tirphycraft.java
+++ src/main/java/laz/tirphycraft/Tirphycraft.java
@@ -5,11 +5,13 @@ import laz.tirphycraft.particle.GlintParticle;
 import net.minecraft.client.Minecraft;
 import net.minecraft.particles.ParticleType;
 import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.EventBus;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import static laz.tirphycraft.Tirphycraft.MOD_ID;
@@ -25,6 +27,11 @@ public class Tirphycraft
     public Tirphycraft() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         TirphycraftRegistries.init(bus);
+        MinecraftForge.EVENT_BUS.addListener(Tirphycraft::serverAboutToStart);
+    }
+
+    private static void serverAboutToStart(FMLServerAboutToStartEvent event) {
+        // DimensionManager.registerOrGetDimension(new ResourceLocation(MOD_ID, "school_grounds"), ACADEMY_GROUNDS.get(), null, true);
     }
 
     @Mod.EventBusSubscriber(bus= Mod.EventBusSubscriber.Bus.MOD)
