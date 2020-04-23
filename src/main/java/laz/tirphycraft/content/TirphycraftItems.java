7a9d06fc2aa61785a9d895b8a02d031f36f9240b
Drizzs <drizzs@hotmail.com>
2020-04-23 11:32:58 -0400

    Teleporter

--- src/main/java/laz/tirphycraft/content/TirphycraftItems.java
+++ src/main/java/laz/tirphycraft/content/TirphycraftItems.java
@@ -1,9 +1,18 @@
 package laz.tirphycraft.content;
 
+import laz.tirphycraft.Teleporter;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+
+import java.util.function.Supplier;
+
+import static laz.tirphycraft.content.TirphycraftRegistries.addItemWClass;
+
 public class TirphycraftItems {
 
     public static void init(){
 
+        addItemWClass("teleporter", Teleporter::new);
 
 
     }
