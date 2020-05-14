9cafaafca0a6681c8f4d1e6a8e8b04a292adb10d
Lazzzzz <37779417+Lazzzzz@users.noreply.github.com>
2020-05-14 20:31:03 +0200

    blizzard...

--- src/main/java/laz/tirphycraft/client/render/weather/BlizzardRendering.java
+++ src/main/java/laz/tirphycraft/client/render/weather/BlizzardRendering.java
@@ -21,21 +21,20 @@ import net.minecraftforge.client.IRenderHandler;
 
 public class BlizzardRendering implements IRenderHandler {
 
-	private static final ResourceLocation RAIN_TEXTURES = new ResourceLocation("textures/environment/rain.png");
 	private static final ResourceLocation SNOW_TEXTURES = new ResourceLocation("textures/environment/snow.png");
 
-	private final float[] rainxs = new float[1024];
-	private final float[] rainys = new float[1024];
+	private final float[] rainxs = new float[16384];
+	private final float[] rainys = new float[16384];
 
 	private int rendererUpdateCount;
 
 	public BlizzardRendering() {
-		for (int i = 0; i < 32; ++i) {
-			for (int j = 0; j < 32; ++j) {
-				float f = (float) (j - 16);
-				float f1 = (float) (i - 16);
+		for (int i = 0; i < 128; ++i) {
+			for (int j = 0; j < 128; ++j) {
+				float f = (float) (j - 64);
+				float f1 = (float) (i - 64);
 				float f2 = MathHelper.sqrt(f * f + f1 * f1);
-				this.rainxs[i << 5 | j] = -f1 / f2;
+				this.rainxs[i << 5 | j] = f1 / f2;
 				this.rainys[i << 5 | j] = f / f2;
 			}
 		}
@@ -55,6 +54,7 @@ public class BlizzardRendering implements IRenderHandler {
 	private void renderNormalWeather(float partialTicks, Minecraft mc) {
 		float f = mc.world.getRainStrength(partialTicks);
 		Vec3d vec3d = mc.gameRenderer.getActiveRenderInfo().getProjectedView();
+
 		double xIn = vec3d.getX();
 		double yIn = vec3d.getY();
 		double zIn = vec3d.getZ();
@@ -62,19 +62,22 @@ public class BlizzardRendering implements IRenderHandler {
 		if (!(f <= 0.0F)) {
 			mc.gameRenderer.getLightTexture().enableLightmap();
 			World world = mc.world;
+
 			int i = MathHelper.floor(xIn);
 			int j = MathHelper.floor(yIn);
 			int k = MathHelper.floor(zIn);
+
 			Tessellator tessellator = Tessellator.getInstance();
 			BufferBuilder bufferbuilder = tessellator.getBuffer();
+
 			RenderSystem.disableCull();
 			RenderSystem.normal3f(0.0F, 1.0F, 0.0F);
 			RenderSystem.enableBlend();
 			RenderSystem.defaultBlendFunc();
 			RenderSystem.defaultAlphaFunc();
-			int l = 5;
+			int l = 20;
 			if (mc.gameSettings.fancyGraphics) {
-				l = 10;
+				l = 40;
 			}
 
 			int i1 = -1;
@@ -84,13 +87,17 @@ public class BlizzardRendering implements IRenderHandler {
 
 			for (int j1 = k - l; j1 <= k + l; ++j1) {
 				for (int k1 = i - l; k1 <= i + l; ++k1) {
-					int l1 = (j1 - k + 16) * 32 + k1 - i + 16;
+					int l1 = (j1 - k + 64) * 64 + k1 - i + 64;
+
 					double d0 = (double) this.rainxs[l1] * 0.5D;
 					double d1 = (double) this.rainys[l1] * 0.5D;
+
 					blockpos$mutableblockpos.setPos(k1, 0, j1);
+
 					Biome biome = world.getBiome(blockpos$mutableblockpos);
+
 					if (biome.getPrecipitation() != Biome.RainType.NONE) {
-						int i2 = world.getHeight(Heightmap.Type.MOTION_BLOCKING, blockpos$mutableblockpos).getY();
+						int i2 = world.getHeight(Heightmap.Type.WORLD_SURFACE, blockpos$mutableblockpos).getY() - 1;
 						int j2 = j - l;
 						int k2 = j + l;
 						if (j2 < i2) {
@@ -111,94 +118,61 @@ public class BlizzardRendering implements IRenderHandler {
 									(long) (k1 * k1 * 3121 + k1 * 45238971 ^ j1 * j1 * 418711 + j1 * 13761));
 							blockpos$mutableblockpos.setPos(k1, j2, j1);
 							float f2 = biome.getTemperature(blockpos$mutableblockpos);
-							if (f2 >= 0.15F) {
-								if (i1 != 0) {
-									if (i1 >= 0) {
-										tessellator.draw();
-									}
 
-									i1 = 0;
-									mc.getTextureManager().bindTexture(RAIN_TEXTURES);
-									bufferbuilder.begin(7, DefaultVertexFormats.PARTICLE_POSITION_TEX_COLOR_LMAP);
+							if (i1 != 1) {
+								if (i1 >= 0) {
+									tessellator.draw();
 								}
 
-								int i3 = this.rendererUpdateCount + k1 * k1 * 3121 + k1 * 45238971 + j1 * j1 * 418711
-										+ j1 * 13761 & 31;
-								float f3 = -((float) i3 + partialTicks) / 32.0F * (3.0F + random.nextFloat());
-								double d2 = (double) ((float) k1 + 0.5F) - xIn;
-								double d4 = (double) ((float) j1 + 0.5F) - zIn;
-								float f4 = MathHelper.sqrt(d2 * d2 + d4 * d4) / (float) l;
-								float f5 = ((1.0F - f4 * f4) * 0.5F + 0.5F) * f;
-								blockpos$mutableblockpos.setPos(k1, l2, j1);
-								int j3 = WorldRenderer.getCombinedLight(world, blockpos$mutableblockpos);
-								bufferbuilder
-										.pos((double) k1 - xIn - d0 + 0.5D, (double) k2 - yIn,
-												(double) j1 - zIn - d1 + 0.5D)
-										.tex(0.0F, (float) j2 * 0.25F + f3).color(1.0F, 1.0F, 1.0F, f5).lightmap(j3)
-										.endVertex();
-								bufferbuilder
-										.pos((double) k1 - xIn + d0 + 0.5D, (double) k2 - yIn,
-												(double) j1 - zIn + d1 + 0.5D)
-										.tex(1.0F, (float) j2 * 0.25F + f3).color(1.0F, 1.0F, 1.0F, f5).lightmap(j3)
-										.endVertex();
-								bufferbuilder
-										.pos((double) k1 - xIn + d0 + 0.5D, (double) j2 - yIn,
-												(double) j1 - zIn + d1 + 0.5D)
-										.tex(1.0F, (float) k2 * 0.25F + f3).color(1.0F, 1.0F, 1.0F, f5).lightmap(j3)
-										.endVertex();
-								bufferbuilder
-										.pos((double) k1 - xIn - d0 + 0.5D, (double) j2 - yIn,
-												(double) j1 - zIn - d1 + 0.5D)
-										.tex(0.0F, (float) k2 * 0.25F + f3).color(1.0F, 1.0F, 1.0F, f5).lightmap(j3)
-										.endVertex();
-							} else {
-								if (i1 != 1) {
-									if (i1 >= 0) {
-										tessellator.draw();
-									}
-
-									i1 = 1;
-									mc.getTextureManager().bindTexture(SNOW_TEXTURES);
-									bufferbuilder.begin(7, DefaultVertexFormats.PARTICLE_POSITION_TEX_COLOR_LMAP);
-								}
-
-								float f6 = -((float) (this.rendererUpdateCount & 511) + partialTicks) / 512.0F;
-								float f7 = (float) (random.nextDouble()
-										+ (double) f1 * 0.01D * (double) ((float) random.nextGaussian()));
-								float f8 = (float) (random.nextDouble()
-										+ (double) (f1 * (float) random.nextGaussian()) * 0.001D);
-								double d3 = (double) ((float) k1 + 0.5F) - xIn;
-								double d5 = (double) ((float) j1 + 0.5F) - zIn;
-								float f9 = MathHelper.sqrt(d3 * d3 + d5 * d5) / (float) l;
-								float f10 = ((1.0F - f9 * f9) * 0.3F + 0.5F) * f;
-								blockpos$mutableblockpos.setPos(k1, l2, j1);
-								int k3 = WorldRenderer.getCombinedLight(world, blockpos$mutableblockpos);
-								int l3 = k3 >> 16 & '\uffff';
-								int i4 = (k3 & '\uffff') * 3;
-								int j4 = (l3 * 3 + 240) / 4;
-								int k4 = (i4 * 3 + 240) / 4;
-								bufferbuilder
-										.pos((double) k1 - xIn - d0 + 0.5D, (double) k2 - yIn,
-												(double) j1 - zIn - d1 + 0.5D)
-										.tex(0.0F + f7, (float) j2 * 0.25F + f6 + f8).color(1.0F, 1.0F, 1.0F, f10)
-										.lightmap(k4, j4).endVertex();
-								bufferbuilder
-										.pos((double) k1 - xIn + d0 + 0.5D, (double) k2 - yIn,
-												(double) j1 - zIn + d1 + 0.5D)
-										.tex(1.0F + f7, (float) j2 * 0.25F + f6 + f8).color(1.0F, 1.0F, 1.0F, f10)
-										.lightmap(k4, j4).endVertex();
-								bufferbuilder
-										.pos((double) k1 - xIn + d0 + 0.5D, (double) j2 - yIn,
-												(double) j1 - zIn + d1 + 0.5D)
-										.tex(1.0F + f7, (float) k2 * 0.25F + f6 + f8).color(1.0F, 1.0F, 1.0F, f10)
-										.lightmap(k4, j4).endVertex();
-								bufferbuilder
-										.pos((double) k1 - xIn - d0 + 0.5D, (double) j2 - yIn,
-												(double) j1 - zIn - d1 + 0.5D)
-										.tex(0.0F + f7, (float) k2 * 0.25F + f6 + f8).color(1.0F, 1.0F, 1.0F, f10)
-										.lightmap(k4, j4).endVertex();
+								i1 = 1;
+								mc.getTextureManager().bindTexture(SNOW_TEXTURES);
+								bufferbuilder.begin(7, DefaultVertexFormats.PARTICLE_POSITION_TEX_COLOR_LMAP);
 							}
+
+							float f6 = -((float) (this.rendererUpdateCount & 511) + partialTicks) / 512.0F;
+
+							float f7 = (float) (random.nextDouble()
+									+ (double) f1 * 0.01D * (double) ((float) random.nextGaussian()));
+							float f8 = (float) (random.nextDouble()
+									+ (double) (f1 * (float) random.nextGaussian()) * 0.001D);
+
+							double d3 = (double) ((float) k1 + 0.5F) - xIn;
+							double d5 = (double) ((float) j1 + 0.5F) - zIn;
+
+							float f9 = MathHelper.sqrt(d3 * d3 + d5 * d5) / (float) l;
+							float f10 = ((1.0F - f9 * f9) * 0.3F + 0.5F) * f;
+
+							int k3 = WorldRenderer.getCombinedLight(world, blockpos$mutableblockpos);
+
+							int l3 = k3 >> 16 & '\uffff';
+							int i4 = (k3 & '\uffff') * 3;
+							int j4 = (l3 * 3 + 240) / 4;
+							int k4 = (i4 * 3 + 240) / 4;
+							bufferbuilder
+									.pos((double) k1 - xIn - d0 + 0.5D, (double) k2 - yIn,
+											(double) j1 - zIn - d1 + 0.5D)
+									.tex(0.0F + f7, (float) j2 * 0.25F + f6 + f8).color(1.0F, 1.0F, 1.0F, f10)
+									.lightmap(k4, j4).endVertex();
+
+							bufferbuilder
+									.pos((double) k1 - xIn + d0 + 0.5D, (double) k2 - yIn,
+											(double) j1 - zIn + d1 + 0.5D)
+									.tex(1.0F + f7, (float) j2 * 0.25F + f6 + f8).color(1.0F, 1.0F, 1.0F, f10)
+									.lightmap(k4, j4).endVertex();
+
+							bufferbuilder
+									.pos((double) k1 - xIn + d0 + 0.5D, (double) j2 - yIn,
+											(double) j1 - zIn + d1 + 0.5D)
+									.tex(1.0F + f7, (float) k2 * 0.25F + f6 + f8).color(1.0F, 1.0F, 1.0F, f10)
+									.lightmap(k4, j4).endVertex();
+
+							bufferbuilder
+									.pos((double) k1 - xIn - d0 + 0.5D, (double) j2 - yIn,
+											(double) j1 - zIn - d1 + 0.5D)
+									.tex(0.0F + f7, (float) k2 * 0.25F + f6 + f8).color(1.0F, 1.0F, 1.0F, f10)
+									.lightmap(k4, j4).endVertex();
 						}
+
 					}
 				}
 			}
